1dbbd5677eb951abb5c958b1a7a5e9ac8932f0ac
Jason Cross <jason@hellojason.net>
2016-06-23 21:39:02 -0500

    GitHub components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Jason Cross',
+  username: 'hello-jason',
+  image: 'http://placehold.it/200x200'
+}
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function() {
     return (
-      <div>Hello Jason</div>
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
