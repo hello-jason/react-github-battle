a1a222184771ee822862e229d60875ecd5f7b6a5
Jason Cross <jason@hellojason.net>
2016-06-24 15:04:22 -0500

    Adds react-router and sets up Main and Home routes.

--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var USER_DATA = {
-  name: 'Jason Cross',
-  username: 'hello-jason',
-  image: 'http://placehold.it/200x200'
-}
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <a href={'https://github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+var routes = require('./config/routes')
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
